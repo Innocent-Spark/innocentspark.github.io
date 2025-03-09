9c96b5b5b14817b20a82a118c3376f13ff7a0f29
Joseph Finlayson <joseph.finlayson@gmail.com>
2025-03-09 11:22:12 +0100

    render pics dynamically

--- .eleventy.js
+++ .eleventy.js
@@ -6,12 +6,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("CNAME");
 
+  // Add a debug filter for templates
+  eleventyConfig.addFilter("debug", (content) => {
+    return `<pre>${JSON.stringify(content, null, 2)}</pre>`;
+  });
+
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes",
-      layouts: "_layouts"
+      layouts: "_layouts",
+      data: "_data"
     },
     templateFormats: ["html", "njk", "md"],
     htmlTemplateEngine: "njk",
